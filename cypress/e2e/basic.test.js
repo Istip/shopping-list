78a05d74f2d4bf3ac98e57275a04e4c40e9c7c34
Istip <isticsek@gmail.com>
2022-07-02 14:14:37 +0300

    started writing tests

--- cypress/e2e/basic.test.js
+++ cypress/e2e/basic.test.js
@@ -1,6 +1,16 @@
-describe('FIRST TEST SUITE', () => {
-  it('App should start', () => {
+describe('BASIC TEST SUITE', () => {
+  beforeEach('Visit the main route', () => {
     cy.visit('/');
+  });
+
+  it('App should start', () => {
     cy.get('[data-cy="main-title"]').should('contain', 'Our List');
+    cy.get('button').first().should('contain', 'Show');
+    cy.get('button').last().should('contain', 'Add to List');
+  });
+
+  it('You can type to the input field', () => {
+    cy.get('[placeholder="Enter item name.."]').type('Item');
+    cy.get('[placeholder="Enter item name.."]').should('have.value', 'Item');
   });
 });
